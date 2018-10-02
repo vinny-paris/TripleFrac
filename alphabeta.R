df42c309b61669d6ade0e7f1f2875b8a5cd8bba9
vinny-paris <vinny@iastate.edu>
2018-10-01 22:14:21 -0500

    gives names that allow for naming conventions

---
+++ alphabeta.R
@@ -0,0 +1,5 @@
+greek <- paste(LETTERS, '2_', sep = "")
+russian <- paste(LETTERS, '_', sep = "")
+latin <- data.frame("", russian, greek)
+names(latin) <- c("Level 0", "Level 1", "Level 2")
+latin]
