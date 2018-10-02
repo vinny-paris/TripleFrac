6aba291f9894ef4c769e4d7fd58af9234b1c41ae
vinny-paris <vinny@iastate.edu>
2018-10-01 22:19:54 -0500

    cleaned up

---
+++ alias_design.R
@@ -0,0 +1,26 @@
+alias_design <- function(x){
+
+  
+  books <- NULL
+  for(i in 1:x){
+    
+    col <- c(kronecker(c(rep(c(0,1,2), alias_dim_calc(x-i)-1), 1), rep(1, 3^(i - 1))), rep(0, (3^(i-1) + 1)/2 - 1))
+    
+    books <- cbind(col, books)
+    
+    
+    
+  }
+    
+
+  return(books)
+}
+  
+  
+  
+  
+  
+  
+  
+  
+  
