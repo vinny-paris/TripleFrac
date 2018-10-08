509343e6c1cfeadb0f16ef60faa950647986af75
vinny-paris <vinny@iastate.edu>
2018-10-07 19:55:22 -0500

    new and imporved examples

--- inst/tripling_ex.R
+++ inst/tripling_ex.R
@@ -3,7 +3,7 @@ l <- matrix(c(1,2,0,1,0,1,2,0), nrow = 2, byrow = TRUE)
 trees <- c(1,1)
 h <- part(x, l, trees)[[1]]
 
-l <- matrix(c(1,2,0,0,0,1,2,0,0,0,1,2), nrow = 3, byrow = TRUE)
+l <- matrix(c(1,2,0,0,0,1,2,0,0,0,1,1), nrow = 3, byrow = TRUE)
 
 
 what_frac(h)
