26441429e1e82a82df73fc00a14d99f6c014c942
vinny-paris <vinny@iastate.edu>
2018-11-04 19:40:19 -0600

    documentation

--- R/alias_design.R
+++ R/alias_design.R
@@ -1,10 +1,16 @@
-#Gives all possible equations for how a factorial experiment at 3 levels may be split upon.
+#' All Factor Combinations 
+#' 
+#' This will produce a design matrix for a non-fractional factorial experiment with n number of factors at 3 levels
+#' 
+#' @export
+#' @param n A natural number, the number of factors in the experiment
+#' @return Returns a matrix with n columns. All entries will be 0, 1, or 2. This will be the "perfect" design matrix for an experiment with these many factors.
 
-alias_design <- function(x){
+alias_design <- function(n){
 
   
   books <- NULL
-  for(i in 1:x){
+  for(i in 1:n){
     
     col <- c(kronecker(c(rep(c(0,1,2), alias_dim_calc(x-i)-1), 1), rep(1, 3^(i - 1))), rep(0, (3^(i-1) + 1)/2 - 1))
     
