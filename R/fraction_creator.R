8d4b8d32384de673dcf864dd63fef455ace2e5fc
vinny-paris <vinny@iastate.edu>
2018-11-06 13:18:59 -0600

    documentation

--- R/fraction_creator.R
+++ R/fraction_creator.R
@@ -1,6 +1,15 @@
-# x is the var identifiers you want
-# l is the single equation you want to generate the fraction from
-# tree is (a number and) the particular branch of the trianary tree you want (i.e. 1*x + 2*y + 0*z mod 3 = tree)
+#' Creates a 3^(f-1) Regular Fraction
+#' 
+#' This will create a regular fractional factorial design by solving a single generating equation.
+#' 
+#' @export
+#' @param x This is the identifiers for the design factors. It should be a vector with each element being a character string or reducable to a character string. Needs to be of f length.
+#' @param l This is the single equation that the new design is to be generated from. It is a vector of f length with each element being 0, 1 or 2
+#' @param tree A single number that is 0, 1, or 2 which will be the solution to equation from l (i.e. 1*x + 2*y + 0*z mod 3 = tree). This dictates the fraction you want.
+#' 
+#' @return A list will be returned. 
+#' \item{Short Design} A matix. This will return all relevant factors and levels as solutions to the design discluding those factors which simply "cycle through" the design matrix. 
+#' \item{Expansion Variables} A vector. The short design should be copied three times, with a new column for the expansion variable. For each copy increase the expansion variables level by one starting at 0. Continue to grow as needed for as many variables as there are in this category.
 
 part_single <- function(x, l, tree){
   
