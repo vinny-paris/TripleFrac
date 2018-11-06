249f12a27c61302f735ef3a1cc4e9ee368228570
vinny-paris <vinny@iastate.edu>
2018-11-06 13:47:32 -0600

    Better parameterization and documentation

--- R/mutl_fraction_creator.R
+++ R/mutl_fraction_creator.R
@@ -1,8 +1,19 @@
-# x is the var identifiers you want
-# l is a matrix of equations you want to generate the fraction from, filled in by row
-# tree is (a vector and) the particular branch of the trianary tree you want (i.e. 1*x + 2*y + 0*z mod 3 = tree)
+#' Creates a 3^(f-s) Regular Fraction
+#' 
+#' This will create a regular fractional factorial design by solving a system of generating equations.
+#' 
+#' @export
+#' @param ids This is the identifiers for the design factors. It should be a vector with each element being a character string or reducable to a character string. Needs to be of f length.
+#' @param l This is the matrix system of equations that the new design is to be generated from. It is a matrix of f width and s length with each element being 0, 1 or 2. Each row corresponds to one generation equation.
+#' @param tree A vector of numbers that is 0, 1, or 2 which will be the solution to equation from l (i.e. 1*x + 2*y + 0*z mod 3 = tree). This dictates the fraction you want.
+#' 
+#' @return A list will be returned. 
+#' \item{Short Design} A matix. This will return all relevant factors and levels as solutions to the design discluding those factors which simply "cycle through" the design matrix. 
+#' \item{Expansion Variables} A vector. The short design should be copied three times, with a new column for the expansion variable. For each copy increase the expansion variables level by one starting at 0. Continue to grow as needed for as many variables as there are in this category.
 
-part <- function(x, l, trees){
+part <- function(ids, l, trees){
+  
+  x <- ids
   
   #create the correct coef.'s as l_* 
   n <- ifelse(is.vector(l) == TRUE, dim(as.data.frame(l))[1], dim(as.data.frame(l))[2])
