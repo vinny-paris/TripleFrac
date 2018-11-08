21e30bc2ca27bc4f6d1e670580e0973cb05fbd23
vinny-paris <vinny@iastate.edu>
2018-11-07 22:07:17 -0600

    Clean end result and Documentation

--- R/opt_rotation.r
+++ R/opt_rotation.r
@@ -1,3 +1,32 @@
+#' Finds an Optimal Rotation Vector
+#' 
+#' This function will produce an optimal rotation vector for some design. It will produce Resolution IV if possible and if not will find a minimal aberration. If multiple minimal aberration designs exist it will randomly choose one and ignore any "desirability" between factors.
+#' 
+#' @export
+#' 
+#' @param design This needs to be a coded design matrix using (0,1,2) where each row corresponds to one treatment. 
+#' 
+#' @value It will produce a list
+#' \item{Rotation Vector: The first element of the list will be the optimum rotation vector and a small message whether Resoluiton IV is obtainable or if this is minimal abberration}
+#' \item{psuedo-design matrix: This is the fake design matrix which is the coded effects of length 3 that are aliased with the intercept in the original design. This will not neccessarly be a regular fractional factorial design (hence psuedo)}
+#' 
+#' @examples
+#' \dontrun{
+#' x <- c("a", "b", "c", "d", "e", "f")
+l <- matrix(c(1,2,0,1,1,1, 0,1,1,2,0,0, 1,1,1,2,2,0), nrow = 3, byrow = TRUE)
+trees <- c(0,0,0)
+m <- part(x, l, trees)[[1]]
+head(m)
+what_frac(m)
+opt_rotation(m)
+#' }
+
+
+
+
+
+
+
 
 
 opt_rotation <- function(design){
@@ -94,11 +123,17 @@ opt_rotation <- function(design){
     
     ri_num <- rowSums(outs != 0)
     
-    sol <- as.data.frame(holding[min(which(ri_num == max(ri_num))),])
+    sol <- as.vector(as.vector(holding[min(which(ri_num == max(ri_num))),]))
+    
+    colnames(g3) <- NULL
+    
+    result <- list(sol, g3)
+    
+    names(result)[[2]] <- "psuedo-design matrix"
     
     theo_max <- dim(g3)[1]
     
-    if(max(ri_num) == theo_max){ colnames(sol) <- "Resolution IV Obtainable"} else {colnames(sol) <- "Minimum Aberration Achieved"}
+    if(max(ri_num) == theo_max){ names(result)[1]<- "Resolution IV Obtainable"} else {names(result)[1] <- "Minimum Aberration Achieved"}
 
     
   }
@@ -107,7 +142,7 @@ opt_rotation <- function(design){
   
   
   
-  return(sol)
+  return(result)
   
   
 }
