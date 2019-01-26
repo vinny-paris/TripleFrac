865f2dd3c591caa4f7bd50568e46c5f9d8e41403
vinny-paris <vinny@iastate.edu>
2019-01-26 15:28:34 -0600

    updated param names and error messages

--- R/checker.R
+++ R/checker.R
@@ -4,29 +4,39 @@
 #'  
 #' @export
 #'   
-#' @param  g A matrix. This is the coded psuedo-design matrix with each element being 0, 1, or 2. Each row corresponds to one generator.
+#' @param  g3 A matrix. This is the coded psuedo-design matrix with each element being 0, 1, or 2. Each row corresponds to one generator.
 #' @param rotation_vector This is the rotation vector that is to be evaluated. 
 #' 
 #' @return A List. 
 #' \item{rotation vector}{This is the vector you put in from the parameters}
+#' \item{output}{A data frame with one column being the effect and the second col being the level}
+#' 
 #' 
 #' @examples 
 #' \dontrun{
 #' 
-#' q <- c(1, 2, 1, 0, 0, 0,
+#' g3 <- c(1, 2, 1, 0, 0, 0,
        #'       0, 0, 1, 0, 2, 2,
        #'       0, 0, 1, 1, 0, 1,
        #'        0, 0, 1, 2, 1, 1,
        #'        0, 0, 0, 1, 1, 2)
-#' q <- matrix(q, nrow = 5, byrow = TRUE)
-#' checker(q, c(2,1,1,0,1,1))
+#' g3 <- matrix(q, nrow = 5, byrow = TRUE)
+#' checker(g3, c(2,1,1,0,1,1))
 #' }
 #' 
 #' 
 #' 
 
-checker <- function(g, rotation_vector){
-  q <- g
+checker <- function(g3, rotation_vector){
+  if(sum(class(g3) == c("matrix", "data.frame")) == 0 ) {stop("Please give the design as a matrix or data.frame!", call. = FALSE)}
+  if(sum(sort(unique(as.vector(g3))) == c(0, 1, 2)) != 3) {stop("Please code the matrix with 0, 1 and 2's only!")}
+  if(dim(g3)[2] != length(rotation_vector)) {stop("Please make the rotation vector have the same number of factors as the design matrix!")}
+  if(sum(class(rotation_vector) == c("numeric", "vector")) == 0) {stop("Please make the rotation vector a numeric vector!")}
+  
+  
+  
+  
+  q <- g3
   x <- rotation_vector
   q %*% x -> l
   l <- l %% 3
@@ -45,7 +55,7 @@ checker <- function(g, rotation_vector){
   }
   
   h <- suppressWarnings(data.frame(holding, l))
-  names(h) <- c('levels', 'origin')
+  names(h) <- c('Effect', 'Level')
   
   h <- list(x, h)
   
